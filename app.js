f5bbc7510afd5b0adf04900936a658ccee4fe422
chandra shekhar <chandrashekhar7at@gmail.com>
2024-09-19 00:29:58 +0530

    link added

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(cors({
-    origin:['http://localhost:5173'],
+    origin:['https://spintowin-sigma.vercel.app'],
     credentials:true
 }))
 
